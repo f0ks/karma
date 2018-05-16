da7479dbefbf115659c36161896b05f8c9e1cfb1
Albert Khamidullin <albert.khm@gmail.com>
2018-05-16 15:07:22 +0300

    initial

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Hapi = require('hapi');
+const mongojs = require('mongojs');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    port: 3000
+});
+
+//Connect to db
+server.app.db = mongojs('karma', ['comments']);
+
+//Load plugins and start server
+server.register([
+    require('./routes/comments')
+], (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    // Start the server
+    server.start((err) => {
+        console.log('Server running at:', server.info.uri);
+    });
+
+});
