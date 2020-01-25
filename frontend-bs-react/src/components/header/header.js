7e1267ed1a366f190a3b9391a574548d8a247e6f
Albert Khamidullin <albert.khm@gmail.com>
2020-01-25 20:14:39 +0300

    bootstrap added

---
+++ frontend-bs-react/src/components/header/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Active</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </header>
+
+    );
+  }
+}
